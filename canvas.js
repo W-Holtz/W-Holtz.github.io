a8d7fde845bb008d3283e7bc811031094a6fe4c8
williamholtz <Willholtz2001@gmail.com>
2024-01-23 20:52:00 -0600

    Background is tied to screen

--- canvas.js
+++ canvas.js
@@ -1,9 +1,16 @@
 //import Road from './road.js'
 //import Renderer from './renderer.js'
 
+// Start by deciding if we are on desktop or mobile:
 let canvas = document.querySelector('canvas');
+var canvasStyle = window.getComputedStyle(canvas);
+const scale = (canvasStyle.scale === "none") ? 1 : canvasStyle.scale; //get
+const mobile = (canvasStyle.scale === "1.5")
+// Do some basic setup
 canvas.width = screen.availWidth;
 canvas.height = screen.availHeight;
+let lazyWindowOffsetX = window.screenX;
+let lazyWindowOffsetY = window.screenY;
 const ctx = canvas.getContext('2d');
 ctx.imageSmoothingEnabled=false;
 ctx.webkitImageSmoothingEnabled = false;
@@ -28,10 +35,21 @@ let frame = 0;
 let stagger = 7;
 let start = Date.now();
 */
+let seconds = 0;
+let oldTimeStamp = 0;
+let frameSpeed = 0;
+let timeStamp = 0;
+
+const x = window.getScreenDetails;
 const backdrop = new Image();
 backdrop.src = './MountainsBase.png';
 const backdropW = 518;
 const backdropH = 518;
+let originX = ((canvas.width)/2) - lazyWindowOffsetX - (backdropW/2);
+let originY = ((canvas.height)/2) - lazyWindowOffsetY - (backdropH/2);
+const screenWhipScalar = 6; // Increase for slower whip
+
+console.log(scale);
 
 function run() {
     let done = false;
@@ -39,16 +57,24 @@ function run() {
     //animate()
 }
 
-
 function draw() {
-    let scalar = 4;
+    // Time logging
+    secondsPassed = (timeStamp - oldTimeStamp) / 1000;
+    oldTimeStamp = timeStamp;
+
+    // Context updating
+    lazyWindowOffsetX += (window.screenX/scale - lazyWindowOffsetX) / screenWhipScalar;
+    lazyWindowOffsetY += (window.screenY/scale - lazyWindowOffsetY) / screenWhipScalar;
+    originX = ((canvas.width/2) - lazyWindowOffsetX - (backdropW/2));
+    originY = ((canvas.height/2) - lazyWindowOffsetY - (backdropH/2));
+
+
+    // Drawing
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "#90956c";
     ctx.fillRect(0,0,canvas.width,canvas.height);
     //background
-    const backdropOriginX = (canvas.width/2) - (scalar*backdropW/2);
-    const backdropOriginY = (canvas.height/2) - (scalar*backdropH/2);
-    ctx.drawImage(backdrop,backdropOriginX,backdropOriginY,backdropW*scalar,backdropH*scalar);
+    ctx.drawImage(backdrop,originX,originY);
     requestAnimationFrame(draw);
 }
 
